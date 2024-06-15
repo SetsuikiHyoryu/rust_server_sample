2f774a323656d16cae451270a6aee98ce5c82829
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2024-06-15 21:43:48 +0900

    feat: create tcpserver

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # Rust 全栈式开发 Web 应用
 
-## 1. 创建 Workspace
+## 目录结构
+
+| 目录/文件 | 说明       |
+| --------- | ---------- |
+| client    | 客户端目录 |
+| server    | 服务端目录 |
+
+## 工作流
+
+### 1. 创建 Workspace
 
 参考：<https://kaisery.github.io/trpl-zh-cn/ch14-03-cargo-workspaces.html>
 
 - 在 `Cargo new` 创建的目录下再次调用 `Cargo new` 创建子目录，则第一层目录会被视为 Workspace。
 - 只在第一层目录内有 `Cargo.lock` 文件。
+- 使用 `Cargo run --package <package_name>` 来指定运行的包。
+  - 参考：<https://doc.rust-lang.org/cargo/commands/cargo-run.html#package-selection>。
 
-## 1. 构建 TCP 服务器
+### 2. 构建 TCP 服务器
 
 - `std::net`: 标准库模块，提供 TCP 和 UDP 通信。
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> Result<()> {
+    // > After creating a TcpListener by binding it to a socket address, it listens for incoming TCP connections. These can be accepted by calling accept or by iterating over the Incoming iterator returned by incoming.
+    // > The socket will be closed when the value is dropped.
+    // See: https://doc.rust-lang.org/std/net/struct.TcpListener.html
+    //
+    // “?” 是错误传播的简写。
+    // See: https://kaisery.github.io/trpl-zh-cn/ch09-02-recoverable-errors-with-result.html#%E4%BC%A0%E6%92%AD%E9%94%99%E8%AF%AF%E7%9A%84%E7%AE%80%E5%86%99-%E8%BF%90%E7%AE%97%E7%AC%A6
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    println!("Running on port 8000...");
+
+    // `listener.accept()` 只会接受一次连接。
+    // `listener.incoming()` 返回一个迭代器，这个迭代器会监听每个连接。
+    // 每个连接都是一个字节流，其类型为 `TcpStream`。
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+    let _stream = stream;
+    println!("Connection established!")
 }
